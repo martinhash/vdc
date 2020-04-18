e8bd1f5630776266c2ebe9da3e2d37467bf1ebee
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-18 16:01:53 -0600

    base

---
+++ src/app/videochat/vdc-home-room/vdc-home-room.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vdc-home-room',
+  templateUrl: './vdc-home-room.component.html',
+  styleUrls: ['./vdc-home-room.component.scss']
+})
+export class VdcHomeRoomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/videochat/vdc-input-chat/vdc-input-chat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vdc-input-chat',
+  templateUrl: './vdc-input-chat.component.html',
+  styleUrls: ['./vdc-input-chat.component.scss']
+})
+export class VdcInputChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
