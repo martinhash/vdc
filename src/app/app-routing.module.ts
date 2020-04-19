8d4d669cc3cc266322244d99af20446030958844
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-19 13:17:30 -0600

    base vdc-home-room

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { VdcPreviewComponent } from './videochat/vdc-preview/vdc-preview.component';
+import { VdcHomeRoomComponent } from './videochat/vdc-home-room/vdc-home-room.component';
 
 
 const routes: Routes = [
   {
     path:'vdcpreview',
     component: VdcPreviewComponent
+  },
+  {
+    path:'vdchomeroom',
+    component: VdcHomeRoomComponent
   },
   {
     path:'**',
